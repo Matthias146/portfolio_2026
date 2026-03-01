2cb8487e7ba08c7b6b76f887986640e83dfd32aa
Matthias Hammelehle <209659387+Matthias146@users.noreply.github.com>
2026-03-01 20:16:33 +0100

    feat(router): add project details route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Projects } from './features/projects/pages/projects/projects';
 import { Home } from './features/home/pages/home/home';
+import { ProjectDetails } from './features/projects/pages/project-details/project-details';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'projects', component: Projects },
+  { path: 'projects/:slug', component: ProjectDetails },
   { path: '**', redirectTo: '' },
 ];
